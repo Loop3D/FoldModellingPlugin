636cc41cf2e5b5fbc31698b26a7a0f1df7bffa56
Kodibarba <50892556+Kodibarba@users.noreply.github.com>
2023-07-04 13:05:26 +1000

    refactored VonMisesFisher class

--- fold_modelling_plugin/objective_functions/von_mises_fisher.py
+++ fold_modelling_plugin/objective_functions/von_mises_fisher.py
@@ -1,39 +1,44 @@
-from scipy.stats import vonmises_fisher
+from typing import List
+rom scipy.stats import vonmises_fisher
 import numpy as np
 
 
 class VonMisesFisher:
     """
-    Class defines objective functions based on Von MisesFisher (vMF) distribution
-    to be used in MLE optimisation problems, and drawing samples from vMF distribution.
-
+    This class defines objective functions based on the Von Mises-Fisher (vMF) distribution.
+    It can be used in Maximum Likelihood Estimation (MLE) optimization problems,
+    and for drawing samples from a vMF distribution.
     """
 
-    def __init__(self, mu, kappa):
+    def __init__(self, mu: List[float], kappa: float):
         """
-        initialise the von Mises-Fisher distribution class.
+        Initialise the Von Mises-Fisher distribution class.
 
         Parameters
         ----------
-        mu : Mean direction vector, in the form [mu_x, mu_y, mu_z].
-        kappa : Concentration parameter (always positive).
+        mu : List[float]
+            Mean direction vector, in the form [mu_x, mu_y, mu_z].
+        kappa : float
+            Concentration parameter (always positive).
 
         Raises
         ------
         ValueError
             If `mu` is not a three-element vector or `kappa` is not a positive number.
         """
+        # Check if mu is a three-element vector
         if len(mu) != 3:
             raise ValueError("`mu` should be a three-element vector.")
+        # Check if kappa is a positive number
         if kappa <= 0:
             raise ValueError("`kappa` should be a positive number.")
 
-        # initialise the the Von Mises Fisher distribution
+        # Initialise the Von Mises Fisher distribution
         self.vmf = vonmises_fisher(mu, kappa)
 
-    def pdf(self, x):
+    def pdf(self, x: np.ndarray) -> np.ndarray:
         """
-        Evaluate the PDF of the von Mises-Fisher distribution at a given point.
+        Evaluate the Probability Density Function (PDF) of the Von Mises-Fisher distribution at a given point.
 
         Parameters
         ----------
@@ -43,58 +48,63 @@ class VonMisesFisher:
         Returns
         -------
         np.ndarray
-            The evaluated PDF.
+            The evaluated PDF at each point in `x`.
         """
-        pdf = self.vmf.pdf(x)
+        # Evaluate the PDF of the Von Mises-Fisher distribution at each point in `x`
+        pdf_values = self.vmf.pdf(x)
 
-        return pdf
+        return pdf_values
 
-    def logpdf(self, x):
+    def logpdf(self, x: np.ndarray) -> np.ndarray:
         """
-        Evaluate the log of the PDF of the von Mises-Fisher distribution at a given point.
+        Evaluate the logarithm of the Probability Density Function (PDF) of the
+        Von Mises-Fisher distribution at a given point.
 
         Parameters
         ----------
         x : np.ndarray
-            Points at which the log of the PDF should be evaluated.
+            Points at which the logarithm of the PDF should be evaluated.
 
         Returns
         -------
         np.ndarray
-            The evaluated log of the PDF.
-
+            The evaluated logarithm of the PDF at each point in `x`.
         """
+        # Evaluate the natural logarithm of the PDF of the Von Mises-Fisher distribution at each point in `x`
+        logpdf_values = self.vmf.logpdf(x)
 
-        logpdf = self.vmf.logpdf(x)
+        return logpdf_values
 
-        return logpdf
 
-    def draw_samples(self, size=1, random_state=1):
+    def draw_samples(self, size: int = 1, random_state: int = 1) -> np.ndarray:
         """
-        Draw random samples from the von Mises-Fisher distribution.
+        Draw random samples from the Von Mises-Fisher distribution.
 
         Parameters
         ----------
         size : int, optional
-            Number of samples to draw, by default 1.
+            The number of random samples to draw. Default is 1.
         random_state : int, optional
-            Seed for the random number generator, by default 1.
+            The seed for the random number generator. Default is 1.
 
         Returns
         -------
         np.ndarray
-            The drawn samples.
+            The array of drawn random samples. Each sample is a 3D vector
 
         Raises
         ------
         TypeError
             If `size` or `random_state` is not an integer.
         """
+        # Check if `size` is an integer
         if not isinstance(size, int):
             raise TypeError("`size` should be an integer.")
+        # Check if `random_state` is an integer
         if not isinstance(random_state, int):
             raise TypeError("`random_state` should be an integer.")
 
+        # Draw random samples from the Von Mises-Fisher distribution
         random_samples = self.vmf.rvs(size=size, random_state=random_state)
 
         return random_samples
