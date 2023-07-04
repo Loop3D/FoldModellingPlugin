9bb5d131b1cfdc29275a3eb4fe57e04b53da84c0
Kodibarba <50892556+Kodibarba@users.noreply.github.com>
2023-07-04 13:06:46 +1000

    refactored gaussian_log_likelihood function

--- fold_modelling_plugin/objective_functions/gaussian.py
+++ fold_modelling_plugin/objective_functions/gaussian.py
@@ -1,9 +1,13 @@
 import numpy as np
+from typing import Union
 
 
-def gaussian_log_likelihood(b, mu, sigma):
+def gaussian_log_likelihood(b: Union[int, float], mu: Union[int, float], sigma: Union[int, float]) -> float:
     """
     Calculate the log-likelihood of a Gaussian distribution.
+
+    This function calculates the log-likelihood of a Gaussian distribution for a given observed value, mean,
+    and standard deviation. The formula used is the standard formula for the log-likelihood of a Gaussian distribution.
 
     Parameters
     ----------
@@ -16,9 +20,20 @@ def gaussian_log_likelihood(b, mu, sigma):
 
     Returns
     -------
-    Union
+    float
         The calculated log-likelihood.
-    """
-    logpdf = -0.5 * np.log(2 * np.pi * sigma ** 2) - 0.5 * (b - mu) ** 2 / sigma ** 2
 
-    return logpdf
+    Raises
+    ------
+    ValueError
+        If `sigma` is less than or equal to 0.
+    """
+    # Check if sigma is greater than 0
+    if sigma <= 0:
+        raise ValueError("`sigma` should be greater than 0.")
+
+    # Calculate the log-likelihood
+    likelihood = -0.5 * np.log(2 * np.pi * sigma ** 2) - 0.5 * (b - mu) ** 2 / sigma ** 2
+
+    # Return the log-likelihood
+    return likelihood
