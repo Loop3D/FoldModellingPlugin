0b1e400200297063144e7b7a3993a629ec169aed
Kodibarba <50892556+Kodibarba@users.noreply.github.com>
2023-07-07 15:32:21 +1000

    Update fold_optimiser.py

--- fold_modelling_plugin/optimisers/fold_optimiser.py
+++ fold_modelling_plugin/optimisers/fold_optimiser.py
@@ -9,19 +9,49 @@ class FoldOptimiser:
     def __init__(self, folded_foliation_data, bounding_box,
                  opt_type,
                  knowledge_constraints=None):
+        """
+                Constructs all the necessary attributes for the FoldOptimiser object.
 
-
+                Parameters
+                ----------
+                    folded_foliation_data : pd.DataFrame
+                        The data related to folded foliation
+                    bounding_box : nd.array
+                        The bounding box data
+                    opt_type : str
+                        The type of optimisation to be performed, can be 'fourier' or 'axial_surface'
+                    knowledge_constraints : dict, optional
+                        The knowledge constraints data (default is None)
+        """
 
         self.folded_foliation_data = folded_foliation_data
         self.bounding_box = bounding_box
         self.opt_type = opt_type
         self.knowledge_constraints = knowledge_constraints
 
+    def setup_fourier_optimisation(self):
+        """
+        Setup the optimisation problem for the fourier series
+        """
+        pass
+
+    def setup_axial_surface_optimisation(self):
+        """
+        Setup the optimisation problem for the axial surface
+        """
+        pass
+
     def setup_optimisation(self):
         """
         Setup the optimisation problem
         """
-        pass
+        if self.opt_type == 'fourier':
+            self.setup_fourier_optimisation()
+        elif self.opt_type == 'axial_surface':
+            self.setup_axial_surface_optimisation()
+        else:
+            raise ValueError('Optimisation type not recognised, '
+                             'please choose between fourier and axial_surface')
 
     def generate_initial_guess(self):
         """
