819384e554d9c896d05e227eb44a501d21bd145f
Kodibarba <rabii.chaarani@monash.edu>
2023-07-13 17:40:09 +1000

    Update fold_optimiser.py
    made as an abstract class for for the optimisation of fold feature

--- fold_modelling_plugin/optimisers/fold_optimiser.py
+++ fold_modelling_plugin/optimisers/fold_optimiser.py
@@ -1,64 +1,24 @@
+from typing import Optional, Dict, Any
+import pandas as pd
 import numpy as np
 from fold_modelling_plugin.input.input_data_checker import CheckInputData
+from base_optimiser import BaseOptimiser
+from abc import ABC, abstractmethod
 
-class FoldOptimiser:
+
+class FoldOptimiser(ABC, BaseOptimiser):
     """
-    Base class for fold geometry optimisation
+    Base class for fold geometry optimisation.
     """
 
-    def __init__(self, folded_foliation_data, bounding_box,
-                 opt_type,
-                 knowledge_constraints=None):
-        """
-                Constructs all the necessary attributes for the FoldOptimiser object.
-
-                Parameters
-                ----------
-                    folded_foliation_data : pd.DataFrame
-                        The data related to folded foliation
-                    bounding_box : nd.array
-                        The bounding box data
-                    opt_type : str
-                        The type of optimisation to be performed, can be 'fourier' or 'axial_surface'
-                    knowledge_constraints : dict, optional
-                        The knowledge constraints data (default is None)
-        """
-
-        self.folded_foliation_data = folded_foliation_data
-        self.bounding_box = bounding_box
-        self.opt_type = opt_type
-        self.knowledge_constraints = knowledge_constraints
-
-    def prepare_knowledge_constraints(self):
+    @abstractmethod
+    def prepare_and_setup_knowledge_constraints(self):
         """
         Prepare the knowledge constraints data
         """
         pass
 
-    def setup_fourier_optimisation(self):
-        """
-        Setup the optimisation problem for the fourier series
-        """
-        pass
-
-    def setup_axial_surface_optimisation(self):
-        """
-        Setup the optimisation problem for the axial surface
-        """
-        pass
-
-    def setup_optimisation(self):
-        """
-        Setup the optimisation problem
-        """
-        if self.opt_type == 'fourier':
-            self.setup_fourier_optimisation()
-        elif self.opt_type == 'axial_surface':
-            self.setup_axial_surface_optimisation()
-        else:
-            raise ValueError('Optimisation type not recognised, '
-                             'please choose between fourier and axial_surface')
-
+    @abstractmethod
     def generate_initial_guess(self):
         """
         Generate an initial guess for the optimisation
@@ -70,9 +30,17 @@ class FoldOptimiser:
 
         pass
 
+    @abstractmethod
+    def setup_optimisation(self):
+        """
+        Setup the optimisation problem
+        """
+        pass
 
-    def optimise(self):
+    @abstractmethod
+    def optimise(self, *args, **kwargs):
         """
         Run the optimisation
         """
+
         pass
