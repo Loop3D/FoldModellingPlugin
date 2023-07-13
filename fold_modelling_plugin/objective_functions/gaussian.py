2369c8fe08d295cd5fa688a35b05a618e3e61ea2
Kodibarba <rabii.chaarani@monash.edu>
2023-07-13 17:37:20 +1000

    Update gaussian.py
    added all loglikelihood functions. Will change name of file later to likelihood

--- fold_modelling_plugin/objective_functions/gaussian.py
+++ fold_modelling_plugin/objective_functions/gaussian.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Union
-
+from utils import fourier_series, get_predicted_rotation_angle
 
 def gaussian_log_likelihood(b: Union[int, float], mu: Union[int, float], sigma: Union[int, float]) -> float:
     """
@@ -37,3 +37,53 @@ def gaussian_log_likelihood(b: Union[int, float], mu: Union[int, float], sigma: 
 
     # Return the log-likelihood
     return likelihood
+
+
+def loglikelihood(y, y_pred):
+    sigma = 1e-2
+    likelihood = gaussian_log_likelihood(y, y_pred, sigma)
+    return likelihood
+
+
+def loglikelihood_axial_surface(x: float) -> Union[int, float]:
+    """
+    Objective function for the axial surface.
+    This function calculates the loglikelihood of an axial surface using the VonMisesFisher distribution.
+
+    Parameters
+    ----------
+    x : float
+        represents the angle between the observed folded foliation and the predicted one.
+
+    Returns
+    -------
+    Union[int, float]
+        The logpdf value from the VonMises distribution.
+    """
+    # Define the mu and kappa of the VonMises distribution
+    # mu = 0 because we want to minimises the angle between the observed and predicted folded foliation
+    # kappa = 100 because we want to have a sharp distribution very close to the mean 0 (mu)
+    mu = 0
+    kappa = 100
+
+    # Create a VonMises distribution with the given parameters
+    vm = vonmises(mu, kappa)
+
+    # Calculate the logpdf of the input array
+    vm_logpdf = vm.logpdf(x)
+
+    return vm_logpdf
+
+
+def loglikelihood_fourier_series(rotation_angle, fold_frame_coordinate):
+
+    def objective_fourier_series(theta):
+        y = np.tan(np.deg2rad(rotation_angle))
+        y_pred = get_predicted_rotation_angle(theta, fold_frame_coordinate)
+        log_likelihood = 0
+        for fr, fd in zip(y, y_pred):
+            log_likelihood += - loglikelihood(fr, fd)
+
+        return log_likelihood
+
+    return objective_fourier_series
