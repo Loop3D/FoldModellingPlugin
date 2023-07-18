12623837844e3cef48fb328e83a0f135f07be78d
Kodibarba <rabii.chaarani@monash.edu>
2023-07-18 14:58:46 +1000

    Update fold_optimiser.py
    big fixes with arguments in setup_optimisation()

--- fold_modelling_plugin/optimisers/fold_optimiser.py
+++ fold_modelling_plugin/optimisers/fold_optimiser.py
@@ -1,9 +1,10 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Union
 import pandas as pd
 import numpy as np
 from fold_modelling_plugin.input.input_data_checker import CheckInputData
 from base_optimiser import BaseOptimiser
 from abc import ABC, abstractmethod
+from fold_modelling_plugin.objective_functions.geological_knowledge import GeologicalKnowledgeFunctions
 
 
 class FoldOptimiser(ABC, BaseOptimiser):
@@ -11,12 +12,40 @@ class FoldOptimiser(ABC, BaseOptimiser):
     Base class for fold geometry optimisation.
     """
 
+    def __init__(self, **kwargs: Dict[str, Any]):
+        """
+        Constructs all the necessary attributes for the Fold Optimiser object.
+
+        Parameters
+        ----------
+            knowledge_constraints : dict, optional
+                The knowledge constraints for the optimiser.
+            kwargs : dict
+                Additional keyword arguments.
+        """
+        self.kwargs = kwargs
+
     @abstractmethod
-    def prepare_and_setup_knowledge_constraints(self):
+    def prepare_and_setup_knowledge_constraints(self, geological_knowledge) ->\
+            Optional[Union[GeologicalKnowledgeFunctions, None]]:
         """
         Prepare the knowledge constraints data
         """
-        pass
+        # Check if knowledge constraints exist
+        if geological_knowledge is not None:
+            # TODO: Add a check if the knowledge constraints are in the correct format
+            # Check if mode is restricted
+            if 'mode' in self.kwargs and self.kwargs['mode'] == 'restricted':
+                geological_knowledge = GeologicalKnowledgeFunctions(geological_knowledge)
+                ready_constraints = geological_knowledge.setup_objective_functions_for_restricted_mode(self)
+
+                return ready_constraints
+            else:
+                geological_knowledge = GeologicalKnowledgeFunctions(geological_knowledge)
+
+                return geological_knowledge
+
+        # If knowledge constraints do not exist, return None
 
     @abstractmethod
     def generate_initial_guess(self):
@@ -31,11 +60,26 @@ class FoldOptimiser(ABC, BaseOptimiser):
         pass
 
     @abstractmethod
-    def setup_optimisation(self):
+    def setup_optimisation(self, geological_knowledge=None) -> tuple:
         """
-        Setup the optimisation problem
+        Setup optimisation.
+
+        Returns
+        -------
+        tuple
+            Returns a tuple containing the geological knowledge objective functions, a solver, and the initial guess.
         """
-        pass
+
+        # Check if method is specified in kwargs and assign the appropriate solver
+        if 'method' in self.kwargs and self.kwargs['method'] == 'differential_evolution':
+            solver = self.optimise_with_differential_evolution
+        else:
+            solver = self.optimise_with_trust_region
+
+        # Prepare and setup knowledge constraints
+        geological_knowledge = self.prepare_and_setup_knowledge_constraints(geological_knowledge)
+
+        return geological_knowledge, solver
 
     @abstractmethod
     def optimise(self, *args, **kwargs):
