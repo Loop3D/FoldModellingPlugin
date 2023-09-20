7f1324ed37f43c4e6e9520dbc3211ca83a363347
Kodibarba <rabii.chaarani@monash.edu>
2023-09-20 11:28:51 +1000

    Squashed commit of the following:
    commit d3c7d56321eb10143915c782779ea282b5e98c87
    Author: Kodibarba <rabii.chaarani@monash.edu>
    Date:   Wed Sep 20 11:27:21 2023 +1000
    
        Update fold_optimiser.py
    
    commit ab262bbf6052e414c8f3de644fc382fbd9c0ac7a
    Author: Kodibarba <rabii.chaarani@monash.edu>
    Date:   Wed Sep 20 11:27:08 2023 +1000
    
        methods moved to FoldOptimiser

--- FoldModellingPlugin/optimisers/base_optimiser.py
+++ FoldModellingPlugin/optimisers/base_optimiser.py
@@ -1,95 +1,34 @@
-from typing import Callable, Dict, Any, Tuple
-
-import numpy as np
-from scipy.optimize import minimize, differential_evolution
-import functools
-from abc import ABC, abstractmethod
-
-# TODO: merge this class with FoldOptimiser
-class BaseOptimiser(ABC):
-    """
-    A base class that to represent an abstract Optimiser.
-
-    ...
-
-    Attributes
-    ----------
-    objective_function : Callable
-        a function which is to be minimised.
-    kwargs : Dict[str, Any]
-        additional keyword arguments.
-
-    Methods
-    -------
-    solve_with_trust_region()
-        Solves the optimisation problem using the trust region method.
-    solve_with_differential_evolution()
-        Solves the optimisation problem using the differential evolution method.
-    """
-
-    @abstractmethod
-    def optimise_with_trust_region(self, objective_function: Callable,
-                                   x0: np.ndarray, constraints=None) -> Dict:
-        """
-        Solves the optimization problem using the trust region method.
-
-        Parameters
-        ----------
-            x0 : np.ndarray
-                Initial guess of the parameters to be optimised.
-
-        Returns
-        -------
-            opt : Dict
-                The solution of the optimisation.
-        """
-
-        opt = minimize(objective_function, x0,
-                       method='trust-constr', jac='2-point',
-                       constraints=constraints, **self.kwargs)
-
-        return opt
-
-    @abstractmethod
-    def optimise_with_differential_evolution(self, objective_function: Callable, bounds: Tuple, init: str = 'halton',
-                                             maxiter: int = 5000, seed: int = 80,
-                                             polish: bool = True, strategy: str = 'best2exp',
-                                             mutation: Tuple[float, float] = (0.3, 0.99)) -> Dict:
-        """
-        Solves the optimization problem using the differential evolution method.
-        Check Scipy documentation for more info
-
-        Parameters
-        ----------
-            bounds : Tuple
-                Bounds for variables. ``(min, max)`` pairs for each element in ``x``,
-                defining the bounds on that parameter.
-            init : str
-                Specify how population initialisation is performed. Default is 'halton'.
-            maxiter : int
-                The maximum number of generations over which the entire population is evolved. Default is 5000.
-            seed : int
-                The seed for the pseudo-random number generator. Default is 80.
-            polish : bool
-                If True (default), then differential evolution is followed by a polishing phase.
-            strategy : str
-                The differential evolution strategy to use. Default is 'best2exp'.
-            mutation : Tuple[float, float]
-                The mutation constant. Default is (0.3, 0.99).
-
-        Returns
-        -------
-            opt : Dict
-                The solution of the optimization.
-                :param objective_function:
-        """
-
-        opt = differential_evolution(objective_function, bounds=bounds, init=init,
-                                     maxiter=maxiter, seed=seed, polish=polish,
-                                     strategy=strategy, mutation=mutation, **self.kwargs)
-
-        return opt
-
-    @abstractmethod
-    def optimise(self, *args, **kwargs) -> Any:
-        pass
+# from typing import Callable, Dict, Any, Tuple
+#
+# import numpy as np
+# from scipy.optimize import minimize, differential_evolution
+# import functools
+# from abc import ABC, abstractmethod
+#
+# # TODO: merge this class with FoldOptimiser
+# class BaseOptimiser(ABC):
+#     """
+#     A base class that to represent an abstract Optimiser.
+#
+#     ...
+#
+#     Attributes
+#     ----------
+#     objective_function : Callable
+#         a function which is to be minimised.
+#     kwargs : Dict[str, Any]
+#         additional keyword arguments.
+#
+#     Methods
+#     -------
+#     solve_with_trust_region()
+#         Solves the optimisation problem using the trust region method.
+#     solve_with_differential_evolution()
+#         Solves the optimisation problem using the differential evolution method.
+#     """
+#
+#
+#
+#     @abstractmethod
+#     def optimise(self, *args, **kwargs) -> Any:
+#         pass
