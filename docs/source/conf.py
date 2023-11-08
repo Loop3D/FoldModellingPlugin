8a400242c8effc79b93f166446f146d437793a2f
Kodibarba <rabii.chaarani@monash.edu>
2023-11-08 15:28:11 +1100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('././'))
+sys.path.insert(0, os.path.abspath('../../'))
 project = 'FoldOptLib'
 copyright = '2023, Rabii Chaarani'
 author = 'Rabii Chaarani'
