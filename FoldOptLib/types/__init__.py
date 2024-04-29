85ded2cc4b388f18161a90463a263aa0f4f7d16a
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-04-29 15:32:24 +0930

    refactor: types to datatypes

--- FoldOptLib/types/__init__.py
+++
@@ -1,3 +0,0 @@
-from probability_distributions import NormalDistribution, VonMisesFisherDistribution
-from _geological_knowledge import InputGeologicalKnowledge
-from enums import KnowledgeType, OptimisationType, LogLikelihoodType, DataType
