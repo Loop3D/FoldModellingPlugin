3f44fe617838fa5d4dda726d53202d1a3f0f20b5
Rabii Chaarani <50892556+rabii-chaarani@users.noreply.github.com>
2024-05-02 15:42:12 +0930

    Update __init__.py

--- FoldOptLib/__init__.py
+++ FoldOptLib/__init__.py
@@ -1,9 +1,9 @@
 # from .fold_modelling import FoldModel, BaseFoldFrameBuilder
-from .datatypes import KnowledgeType, OptimisationType, LikelihoodType, DataType, NormalDistribution, \
+from .datatypes import KnowledgeType, OptimisationType, ObjectiveType, DataType, NormalDistribution, \
     VonMisesFisherDistribution
 from .helper import utils
 from .input import CheckInputData, InputDataProcessor
 from .objective_functions import GeologicalKnowledgeFunctions, VonMisesFisher, LeastSquaresFunctions, \
-    LikelihoodFunction, is_axial_plane_compatible
+    ObjectiveFunction, is_axial_plane_compatible
 from .optimisers import FourierSeriesOptimiser, AxialSurfaceOptimiser
 from .splot import SPlotProcessor
