d16e0dc6d886c25f748753788d1a1ccd35ed3c1e
rabii-chaarani <rabii.chaarani@monash.edu>
2024-05-17 12:34:03 +0930

    ci: added ruff linting in release-please.yml

--- FoldOptLib/__init__.py
+++ FoldOptLib/__init__.py
@@ -1,20 +1,22 @@
-# from .fold_modelling import FoldModel, BaseFoldFrameBuilder
-from .datatypes import (
-    KnowledgeType, 
-    OptimisationType, 
-    ObjectiveType, 
-    DataType, 
+from version import __version__
+from FoldOptLib.datatypes import (
+    KnowledgeType,
+    OptimisationType,
+    ObjectiveType,
+    DataType,
     NormalDistribution,
     VonMisesFisherDistribution,
-    InputGeologicalKnowledge
-    )
-from .utils import utils
-from .input import CheckInputData, InputDataProcessor
-from .objective_functions import (
-    GeologicalKnowledgeFunctions, 
-    VonMisesFisher, 
+    InputGeologicalKnowledge,
+)
+from FoldOptLib.utils import utils
+from FoldOptLib.input import CheckInputData, InputDataProcessor
+from FoldOptLib.objective_functions import (
+    GeologicalKnowledgeFunctions,
+    VonMisesFisher,
     LeastSquaresFunctions,
-    ObjectiveFunction
-    )
-from .optimisers import FourierSeriesOptimiser, AxialSurfaceOptimiser
-from .splot import SPlotProcessor
+    ObjectiveFunction,
+)
+from FoldOptLib.optimisers import FourierSeriesOptimiser, AxialSurfaceOptimiser
+from FoldOptLib.splot import SPlotProcessor
+
+__all__ = [FourierSeriesOptimiser, AxialSurfaceOptimiser]
